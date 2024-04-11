525b7a10b635ca0dd38de0c4ec8289a1846683f2
Thomas Pummer <dev@nullpointer.at>
2024-04-11 19:56:22 +0200

    even simpler test

--- src/test/java/ShowCaseTest.java
+++ src/test/java/ShowCaseTest.java
@@ -1,22 +1,11 @@
-import com.sun.net.httpserver.HttpExchange;
-import org.easymock.EasyMockSupport;
-import org.testng.annotations.AfterMethod;
+import org.easymock.EasyMock;
 import org.testng.annotations.Test;
 
 public class ShowCaseTest {
 
-    private static EasyMockSupport easyMockSupport = new EasyMockSupport();
-    private final HttpExchange httpExchangeMock = easyMockSupport.createMock(HttpExchange.class);
-
-    @AfterMethod(alwaysRun = true)
-    public void afterTest() {
-        easyMockSupport.verifyAll();
-        easyMockSupport.resetAll();
-    }
-
     @Test()
     public void nothing() {
-        easyMockSupport.replayAll();
+        EasyMock.mock(com.sun.net.httpserver.HttpExchange.class);
     }
 
 }
